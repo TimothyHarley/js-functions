4e360f9654777ce291fdc979efc0bcb88fa8424f
Timothy Harley <timothy.d.harley@gmail.com>
2018-08-25 10:27:00 -0500

    object shorthand notation

--- main.js
+++ main.js
@@ -13,3 +13,18 @@ const firstName = 'Tim';
 const lastName = 'Harley';
 greetingElement.innerHTML = 'Hello my name is ' + firstName + ' ' + lastName;
 greetingElement.innerHTML = `Hello my name is ${firstName} ${lastName}`;
+
+//object shorthand notation//
+
+
+let name = "callen";
+let age = '10000000000000';
+//
+//let person = {
+//    name: name,
+//    age: age
+//};
+
+let person = {name, age};
+
+console.log('person:', person.name);
