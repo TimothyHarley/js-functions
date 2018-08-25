726f526aa97d7d42d32469c40c762361ac4dbae9
Timothy Harley <timothy.d.harley@gmail.com>
2018-08-25 11:48:52 -0500

    made writeToDom

--- functions.js
+++ functions.js
@@ -32,5 +32,34 @@ console.log(nomnom('pink','blue')); // pink devoured blue
  //   }
 
 
-    
+ //function numberAdder that takes in a number and
+ //returns the number plus 3.  so input 22=, output =25.
+
+
+ //function numberAdder(number){
+ //    return (number)+3;
+ //};
+ //console.log numberAdder(5);
+
+
+ 
+ const numberAdder = (num) => {
+     const finalNumber = num + 3;
+     printToDom(`<h2>${finalNumber}<h2>`, 'allTheNumbers');
+ };
+
+ 
+ //this is VERY IMPORTANT 
+ const printToDom = (stringToPrint, divId) => {
+     const selectedDiv = document.getElementById(divId);
+     selectedDiv.innerHTML += stringToPrint;
+ };
+//that was VERY IMPORTANT
+
+
+numberAdder(22);
+numberAdder(3);
+numberAdder(45);
+
+
 
